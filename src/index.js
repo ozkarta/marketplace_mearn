9d86537ea762b8595cc0d2923b860ddf4661e199
ozkarta <ozbegi1@gmail.com>
2018-03-20 19:16:10 +0400

    redux auth is done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
+import {createStore} from 'redux'
 
 import Routes from './shared/routes';
 
 import VisitorMainComponent from './user/visitor/visitor';
-
 import SellerMainComponent from './user/seller/seller';
-
 import BuyerMainComponent from './user/buyer/buyer';
-
 import ErrorModal from './shared/error-modal/error-modal';
 import './index.css';
+
+const auth = function (state = {user: { role: 'visitor', user: null, isAuthenticated: false }}, action) {
+    switch (action.type) {
+        case 'LOGIN':
+            return Object.assign({}, state, { 
+                user: {
+                    role: 'buyer', 
+                    user: {
+                        firstName: 'Oz',
+                        lastName: 'kart'
+                    }, 
+                    isAuthenticated: true 
+                }
+            })
+        case 'LOGOUT':
+            return Object.assign({}, state, { 
+                user: {
+                    role: 'visitor', 
+                    user: null, 
+                    isAuthenticated: false 
+                }
+            });
+        default:
+            return state;
+    }
+}
+const store = createStore(auth);
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            error: {
-                error: 'Something Wrong...',
-                errorInfo: 'some info...',
-                hasError: false,
-            },
-            user: {
-                role: 'buyer',
-                user: {},
-                isAuthenticated: true
-            }
-        };
+        this.state = Object.assign(
+            {},
+            props
+        );
     }
 
     componentDidCatch(error, info) {
@@ -40,27 +57,32 @@ class App extends React.Component {
                 <Router>
                     <div>
                         {
-                            !this.state.user.isAuthenticated && <VisitorMainComponent/>                    
+                            !this.state.reduxState.user.isAuthenticated && <VisitorMainComponent/>                    
                         }
                         {
-                            this.state.user.isAuthenticated && this.state.user.role === 'seller' && <SellerMainComponent/>
+                            this.state.reduxState.user.isAuthenticated && this.state.reduxState.user.role === 'seller' && <SellerMainComponent/>
                         }
                         {
-                            this.state.user.isAuthenticated && this.state.user.role === 'buyer' && <BuyerMainComponent/>
+                            this.state.reduxState.user.isAuthenticated && this.state.reduxState.user.role === 'buyer' && <BuyerMainComponent/>
                         }
 
-                       <Routes state={this.state}/>
+                       <Routes {...this.state}/>
 
                     </div>
                 </Router>
 
-                {this.state.error.hasError && <ErrorModal error={this.state.error.error} errorInfo={this.state.error.errorInfo} />}
+                {this.state.error && this.state.error.hasError && <ErrorModal error={this.state.error.error} errorInfo={this.state.error.errorInfo} />}
             </React.Fragment>
         );
     }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+function render() {
+    ReactDOM.render(
+        <App reduxState={store.getState()} store={store}/>,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(render);
+render();
